1862229e11ebd45f05afa411c10301224ee4c70e
Jana Laney <Jana.Laney@gmail.com>
2020-03-24 18:58:20 -0600

    Adding files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Plain');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
